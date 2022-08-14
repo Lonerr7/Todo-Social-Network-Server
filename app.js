0a527c1c1610fffb445e8527cd67b51672c1a628
nic3guy <krabik2342@gmail.com>
2022-08-14 17:24:41 +0300

    3. Handling POST Requests (Users)

--- app.js
+++ app.js
@@ -3,10 +3,16 @@ const fs = require('fs');
 
 const users = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/users.json`));
 
-//* Creating an express app
+//* =================== Creating an express app ===================
+
 const app = express();
 
-//* Routing
+//* =================== Middlewares ===================
+
+app.use(express.json());
+
+//* =================== Routing ===================
+
 app.get('/api/v1/users/', (req, res) => {
   res.status(200).json({
     status: 'success',
@@ -17,7 +23,27 @@ app.get('/api/v1/users/', (req, res) => {
   });
 });
 
-//* Starting a server
+app.post('/api/v1/users', (req, res) => {
+  const newId = users[users.length - 1].id + 1;
+  const newUser = { id: newId, ...req.body };
+
+  users.push(newUser);
+  fs.writeFile(
+    `${__dirname}/dev-data/users.json`,
+    JSON.stringify(users),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          user: newUser,
+        },
+      });
+    }
+  );
+});
+
+//* =================== Starting a server ===================
+
 const port = 8000;
 
 app.listen(port, () => {
