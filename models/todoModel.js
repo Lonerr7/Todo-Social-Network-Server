b1991a1e416dc0f6abcbd588d3dc50cad502276a
nic3guy <krabik2342@gmail.com>
2022-08-16 09:02:58 +0300

    13. Advanced moddeled todos

--- models/todoModel.js
+++ models/todoModel.js
@@ -4,10 +4,22 @@ const todoSchema = new mongoose.Schema({
   taskText: {
     type: String,
     required: [true, 'Please enter a task'],
+    trim: true,
+  },
+  difficulty: {
+    type: String,
+    enum: ['Easy', 'Medium', 'Hard'],
+    default: 'Easy',
+    trim: true,
   },
   isCompleted: {
     type: Boolean,
     default: false,
+  },
+  image: String,
+  createdAt: {
+    type: Date,
+    default: Date.now() + 3 * 60 * 60 * 1000,
   },
 });
 
