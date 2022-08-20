8eb7bc093d0b25146ff5a7c4161ed02811001683
nic3guy <krabik2342@gmail.com>
2022-08-20 08:45:42 +0300

    27. Errors Outside Express: Unhandled Rejections

--- server.js
+++ server.js
@@ -5,13 +5,17 @@ const mongoose = require('mongoose');
 dotenv.config({ path: './config.env' });
 
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DB_PASSWORD);
-mongoose
-  .connect(DB)
-  .then(() => console.log(`DB CONNECTION SUCCESSFUL`))
-  .catch((err) => console.log(err));
+mongoose.connect(DB).then(() => console.log(`DB CONNECTION SUCCESSFUL`));
 
 // Creating a server
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
+});
+
+// Handling unhandled rejections
+process.on('unhandledRejection', (reason) => {
+  console.log(`UNHANDLED REJECTION! 💥 SHUTTING DOWN...`);
+  console.log(`REASON: ${reason.errmsg}`);
+  server.close(() => process.exit(1));
 });
