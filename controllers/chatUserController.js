3d84f952183f2e0644d5e2e95a2c75a76094e919
nic3guy <krabik2342@gmail.com>
2022-11-07 08:00:56 +0300

    74. Created chatUserRoutes, chatUserController

--- controllers/chatUserController.js
+++ controllers/chatUserController.js
@@ -1,1 +1,6 @@
-exports.createChatUser = 
+const ChatUser = require('../models/chatUserModel');
+const { createOne, getAll, deleteOne } = require('./handlerFactory');
+
+exports.getAllChatUsers = getAll(ChatUser);
+// exports.createChatUser = createOne(ChatUser);
+// exports.deleteCurrentUser = deleteOne(ChatUser);
